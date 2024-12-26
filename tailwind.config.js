9d130c4026a75b64f4fd1e05392e852d51b12a2e
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-26 02:53:34 +0000

    chore: dependencies changed and other configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  future: {
-    hoverOnlyWhenSupported: true,
-  },
-  plugins: [],
+	content: ["./src/app/**/*.{js,jsx,ts,tsx}", "./src/components/**/*.{js,jsx,ts,tsx}"],
+	presets: [require("nativewind/preset")],
+	theme: {
+		extend: {},
+	},
+	future: {
+		hoverOnlyWhenSupported: true,
+	},
+	plugins: [],
 };
