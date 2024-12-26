a6df08bebe787c60169046ec5dcedf0769bc1293
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-26 22:28:52 +0000

    chore: refactor configs files and deleted pnpm

--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,11 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname, {
+	isCSSEnabled: true,
+});
 
 config.resolver.unstable_enablePackageExports = true;
-config.resolver.sourceExts.push('sql');
+config.resolver.sourceExts.push("sql");
 
 module.exports = withNativeWind(config, { input: "./src/global.css" });
