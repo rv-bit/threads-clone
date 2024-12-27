772b9e1eeedb7e68f917c08bb26f5b83b09be9b0
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-27 17:59:19 +0000

    chore: added dummy data to test interface + add search bar in home page

--- src/app/(tabs)/activity/index.tsx
+++ src/app/(tabs)/activity/index.tsx
@@ -1,7 +1,10 @@
 import * as schema from "@/drizzle/schema";
 
 import { useEffect, useState } from "react";
-import { Text, View, ScrollView, SafeAreaView } from "react-native";
+import { ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import PostCard from "@/components/ui/pages/post-card";
 
 import { Fetch } from "@/api/post";
 
@@ -20,76 +23,52 @@ export default function Activity() {
 	}, []);
 
 	return (
-		<SafeAreaView className="h-full w-full flex-1 flex-col items-center justify-center gap-5">
-			<ScrollView className="mt-10 h-full w-full">
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
-				<Text>Activity</Text>
+		<SafeAreaView className="flex-1 flex-col items-start justify-center p-5">
+			<ScrollView className="h-full w-full flex-1">
+				<PostCard
+					username="rsvsbb"
+					avatar="https://randomuser.me/api/portraits/men/75.jpg"
+					content="Hello, World! dawoijdwaoi dsj diojwa djioajwojd sjoi djwaoij daoiwj doijas ij"
+					liked={true}
+					images={[
+						"https://cdn.discordapp.com/attachments/1127585380556877897/1279222724840919130/image.png?ex=676e90c0&is=676d3f40&hm=a3dc1554cc7b26e8d5612d410aa6abcec984daffca874d3043ae7a0b0183fbeb&",
+						"https://cdn.discordapp.com/attachments/1127585380556877897/1279222724840919130/image.png?ex=676e90c0&is=676d3f40&hm=a3dc1554cc7b26e8d5612d410aa6abcec984daffca874d3043ae7a0b0183fbeb&",
+						"https://cdn.discordapp.com/attachments/1127585380556877897/1279222724840919130/image.png?ex=676e90c0&is=676d3f40&hm=a3dc1554cc7b26e8d5612d410aa6abcec984daffca874d3043ae7a0b0183fbeb&",
+						"https://cdn.discordapp.com/attachments/1127585380556877897/1279222724840919130/image.png?ex=676e90c0&is=676d3f40&hm=a3dc1554cc7b26e8d5612d410aa6abcec984daffca874d3043ae7a0b0183fbeb&",
+					]}
+				/>
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
 			</ScrollView>
-
-			{/* {posts && (
-				<ScrollView className="w-full flex-1">
-					{posts.map((post) => (
-						<View key={post.id}>
-							<Text>{post.title}</Text>
-							<Text>{post.content}</Text>
-						</View>
-					))}
-				</ScrollView>
-			)} */}
 		</SafeAreaView>
 	);
 }
--- src/app/(tabs)/homepage/index.tsx
+++ src/app/(tabs)/homepage/index.tsx
@@ -1,105 +1,76 @@
 import * as schema from "@/drizzle/schema";
 
 import { useEffect, useState } from "react";
-import { Text, View, ScrollView, SafeAreaView, Pressable } from "react-native";
-import { router } from "expo-router";
+import { View, ScrollView, SafeAreaView } from "react-native";
 
-import { Fetch } from "@/api/post";
+import CreatePostCard from "@/components/ui/pages/create-post";
+import PostCard from "@/components/ui/pages/post-card";
+
+import Input from "@/components/ui/Input";
 
 export default function Home() {
+	const [searchParams, setSearchParams] = useState<string>("");
 	const [posts, setPosts] = useState<schema.Post[] | null>(null);
 
 	useEffect(() => {
-		(async () => {
-			const posts = await Fetch();
-			setPosts(posts || []);
-		})();
+		console.log(searchParams.trim());
 
-		return () => {
-			setPosts(null);
-		};
-	}, []);
+		return () => {};
+	}, [searchParams]);
 
 	return (
-		<SafeAreaView className="flex-1 flex-col items-start justify-center p-5">
-			<Pressable
-				onPress={() => {
-					router.push("/new-post");
-				}}
-				className="h-fit w-full flex-col items-start justify-start gap-2 border-b-2 border-white p-5"
-			>
-				<View className="w-full flex-col items-start justify-center gap-1">
-					<Text className="text-xl font-bold text-white">username</Text>
-					<Text className="text-md text-gray-500">What's new?</Text>
-				</View>
-			</Pressable>
+		<SafeAreaView className="flex-1 flex-col items-start justify-center p-5 px-0">
+			<View className="mt-5 h-fit w-full items-center justify-center bg-[#181818] p-5 pb-2">
+				<Input value={searchParams} onChange={(query) => setSearchParams(query)} placeholder="Search" className="h-14 w-full rounded-xl bg-[#1E1E1E] px-2 text-white" />
+			</View>
 
-			{posts && (
-				<ScrollView className="h-full w-full flex-1">
-					{/* {posts.map((post) => (
-						<View key={post.id}>
-							<Text>{post.title}</Text>
-							<Text>{post.content}</Text>
-						</View>
-					))} */}
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-					<Text>Activity</Text>
-				</ScrollView>
-			)}
+			<ScrollView className="h-full w-full flex-1">
+				<CreatePostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" />
+
+				<PostCard
+					username="rsvsbb"
+					avatar="https://randomuser.me/api/portraits/men/75.jpg"
+					content="Hello, World! dawoijdwaoi dsj diojwa djioajwojd sjoi djwaoij daoiwj doijas i dwaoijd awjoi jdaowdo jawj dawoij doawji jdaiowj jd oawjod jawj doawji doijawd ijawoi jdoawij doiawj odijawoij daw"
+					liked={true}
+					images={[
+						"https://cdn.discordapp.com/attachments/1200437409981157407/1322071165753229322/IMG_0606.png?ex=676f8a38&is=676e38b8&hm=c9bfb7e1b63737718b76eb15b77797b58a2f3eacf26c9ab325d9605a26d69498&",
+						"https://cdn.discordapp.com/attachments/1200437409981157407/1322071165753229322/IMG_0606.png?ex=676f8a38&is=676e38b8&hm=c9bfb7e1b63737718b76eb15b77797b58a2f3eacf26c9ab325d9605a26d69498&",
+						"https://cdn.discordapp.com/attachments/1200437409981157407/1322071165753229322/IMG_0606.png?ex=676f8a38&is=676e38b8&hm=c9bfb7e1b63737718b76eb15b77797b58a2f3eacf26c9ab325d9605a26d69498&",
+						"https://cdn.discordapp.com/attachments/1200437409981157407/1322071165753229322/IMG_0606.png?ex=676f8a38&is=676e38b8&hm=c9bfb7e1b63737718b76eb15b77797b58a2f3eacf26c9ab325d9605a26d69498&",
+					]}
+				/>
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+				<PostCard username="rsvsbb" avatar="https://randomuser.me/api/portraits/men/75.jpg" content="Hello, World!" />
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
