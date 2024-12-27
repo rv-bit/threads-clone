416d79011c23731ef9364dca21f57a5be29744af
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-27 17:56:25 +0000

    chore: refactored the whole create post card again

---
+++ src/components/ui/pages/create-post.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useRouter } from "expo-router";
+import { Image, Pressable, Text, View } from "react-native";
+
+import { cn } from "@/lib/utils";
+
+import Feather from "@expo/vector-icons/Feather";
+import Ionicons from "@expo/vector-icons/Ionicons";
+
+type CreatePostCardProps = {
+	username?: string;
+	avatar?: string;
+
+	className?: string;
+};
+
+const CreatePostCard = (props: CreatePostCardProps) => {
+	const router = useRouter();
+
+	const navigateToCreatePost = (state?: string) => {
+		router.push({
+			pathname: "/new-post",
+		});
+	};
+
+	return (
+		<Pressable
+			onPress={() => {
+				navigateToCreatePost();
+			}}
+			className={cn("w-full border-b-[1px] border-white/10 p-5", props.className)}
+		>
+			<View className="w-full flex-1 flex-row items-start gap-4">
+				<Image source={{ uri: props.avatar }} style={{ width: 35, height: 35, borderRadius: 50, marginTop: 5 }} />
+
+				<View className="flex-1 flex-col items-start gap-3">
+					<View className="gap-1">
+						<Text className="text-md font-extrabold text-white">{props.username || "username"}</Text>
+						<Text className="text-md text-gray-500">What's new?</Text>
+					</View>
+
+					<View className="flex-row items-center justify-start gap-4">
+						<Pressable
+							onPress={() => {
+								console.log("send to new post with state to select image");
+								navigateToCreatePost("select-image");
+							}}
+						>
+							<Ionicons name="images-outline" size={25} color="#636263" />
+						</Pressable>
+
+						<Pressable
+							onPress={() => {
+								console.log("send to new post with state to take photo");
+								navigateToCreatePost("take-photo");
+							}}
+						>
+							<Feather name="camera" size={25} color="#636263" />
+						</Pressable>
+					</View>
+				</View>
+			</View>
+		</Pressable>
+	);
+};
+
+export default CreatePostCard;
