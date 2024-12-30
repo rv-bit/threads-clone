c3f6af12da3f264bf7d92a827aaa0dab4e5e0a75
Robert Vilcu <robertvilcu89@gmail.com>
2024-12-30 00:05:04 +0000

    feat: added a tab to view all posts with filters

--- src/app/(tabs)/activity/index.tsx
+++ src/app/(tabs)/activity/index.tsx
@@ -1,18 +1,101 @@
-import { useEffect, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
-import { Text, ScrollView, View } from "react-native";
+import { Text, ScrollView, View, TouchableHighlight, FlatList, RefreshControl } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { FetchPosts } from "@/api/post";
+import { usePostStore } from "@/stores/usePosts";
+
+import PostCard from "@/components/ui/cards/post-card";
+
+const filters = ["all", "liked", "images"];
+
 export default function Activity() {
+	const { posts, setPosts } = usePostStore();
+	const [currentFilter, setCurrentFilter] = useState<string>("all");
+
+	const [refreshing, setRefreshing] = useState<boolean>(false);
+
+	const handleFetch = useCallback(async () => {
+		const posts = await FetchPosts();
+
+		if (!posts) {
+			return;
+		}
+
+		return posts;
+	}, []);
+
+	const handleRefresh = async () => {
+		const fetchedPosts = await handleFetch();
+		setRefreshing(true);
+
+		const timeout = setTimeout(() => {
+			setRefreshing(false);
+
+			if (!fetchedPosts) {
+				return;
+			}
+			setPosts(fetchedPosts);
+		}, 2000);
+
+		return () => clearTimeout(timeout);
+	};
+
+	const filteredPosts = useMemo(() => {
+		if (!posts || posts.length === 0) return [];
+
+		if (currentFilter === "all") return posts;
+		if (currentFilter === "liked") return posts.filter((post) => post.liked);
+
+		if (currentFilter === "images") {
+			return posts.filter((post) => post.content.length === 0 && Object.keys(post.images).length > 0);
+		}
+	}, [posts, currentFilter]);
+
 	return (
-		<SafeAreaView className="flex-1 flex-col items-start justify-center p-5 px-0">
-			<View className="mt-5 h-fit w-full items-center justify-center bg-[#181818] p-5 pb-2">
-				{/* <Input value={searchParams} onChange={(query) => setSearchParams(query)} placeholder="Search" className="h-14 w-full rounded-xl bg-[#1E1E1E] px-2 text-white" /> */}
-				<Text className="text-2xl font-bold text-white">Activity</Text>
+		<SafeAreaView style={{ flex: 1 }} edges={["top", "left", "right"]}>
+			<View className="w-full items-center justify-center bg-[#181818] p-5">
+				<ScrollView horizontal showsHorizontalScrollIndicator={false} className="w-full flex-row" contentContainerStyle={{ gap: 5 }}>
+					{filters.map((filter) => {
+						return (
+							<TouchableHighlight
+								activeOpacity={0.6}
+								key={filter}
+								className={`rounded-lg p-3 px-10 ${currentFilter === filter ? "border-none bg-[#ffffff]" : "border-[0.1rem] border-white/20 bg-[#1E1E1E]"}`}
+								onPress={() => setCurrentFilter(filter)}
+							>
+								<Text className={`font-bold capitalize ${currentFilter === filter ? "text-black" : "text-white"}`}>{filter}</Text>
+							</TouchableHighlight>
+						);
+					})}
+				</ScrollView>
 			</View>
-			<ScrollView className="h-full w-full flex-1">
-				<Text className="text-2xl font-bold text-white">Activity</Text>
-			</ScrollView>
+			<FlatList
+				style={{ width: "100%", flex: 1, flexGrow: 1 }}
+				data={filteredPosts}
+				contentContainerStyle={{
+					// @ts-ignore
+					color: "white",
+					paddingBottom: 0,
+				}}
+				renderItem={({ item }) => {
+					return (
+						<PostCard
+							key={item.id}
+							id={item.id}
+							username="rsvsbb"
+							avatar="https://randomuser.me/api/portraits/men/75.jpg"
+							content={item.content}
+							images={item.images}
+							imageLinks={item.imageLinks}
+							liked={item.liked}
+						/>
+					);
+				}}
+				keyExtractor={(item) => item.id.toString()}
+				refreshControl={<RefreshControl refreshing={refreshing} onRefresh={handleRefresh} />}
+			/>
 		</SafeAreaView>
 	);
 }
